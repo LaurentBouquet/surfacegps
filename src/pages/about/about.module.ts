9f73603859584d10f0a484268f8efbc19284035d
Laurent BOUQUET <laurent.bouquet@gmail.com>
2017-12-03 19:40:20 +0100

    ajout de commentaires et nettoyage du code

--- src/pages/about/about.module.ts
+++ src/pages/about/about.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'; 
 import { TranslateModule } from '@ngx-translate/core';
 import { IonicPageModule } from 'ionic-angular';
 
