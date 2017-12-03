c8fea963b4c701cc1e62ea04cc5fca4663f14d4e
Laurent BOUQUET <laurent.bouquet@gmail.com>
2017-12-03 20:44:35 +0100

    Ajout de commentaires et nettoyage du code

--- src/pages/about/README.md
+++ src/pages/about/README.md
@@ -1,3 +1,3 @@
-# Login 
+# About 
 
-The Login page renders a login form with email/password by default and an optional username field.
+The About page renders informations about version and author.
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'; 
+import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { IonicPage, NavController, ToastController } from 'ionic-angular';
 
